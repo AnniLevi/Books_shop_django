80dc938732dee65c4865f841d016509e15c4ece1
alevi1555 <alevi1555@gmail.com>
2021-05-21 13:51:21 +0300

    Implemented delail page for room

--- hotel/urls.py
+++ hotel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from hotel import views
 
 urlpatterns = [
-    path('', views.main_page)
+    path('room_detail/<int:room_id>/', views.room_detail, name='room_detail'),
+    path('', views.main_page),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -6,3 +6,9 @@ def main_page(request):
     query_set = Room.objects.all()
     context = {'all_rooms': query_set}
     return render(request, 'hotel/index.html', context=context)
+
+
+def room_detail(request, room_id):
+    room = Room.objects.get(number=room_id)
+    context = {'room': room}
+    return render(request, 'hotel/room_detail.html', context)
