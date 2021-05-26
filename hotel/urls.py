398212f9dcb34c7201f2ff140d71227b9cd79d25
alevi1555 <alevi1555@gmail.com>
2021-05-26 21:25:57 +0300

    Add comment add bootstrap add prefetch_related start use required_http_method

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,5 +4,7 @@ from hotel import views
 urlpatterns = [
     path('room_detail/<int:room_id>/', views.room_detail, name='room-detail'),
     path('room_booking/<int:room_id>/', views.booking_the_room, name='room-booking'),
+    path('ratings/', views.rating_page, name='hotel-rating'),
+    path('add_rating/', views.add_rating, name='add-rating'),
     path('', views.hotel_page, name='hotel-page'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -29,3 +29,19 @@ def booking_the_room(request, room_id):
     room.save()
     return redirect('hotel-page')
 
+
+def rating_page(request):
+    return render(request, 'hotel/ratings.html')
+
+
+@login_required
+def add_rating(request):
+    # Rating.objects.get(rating_person=request.user.id)
+    new_rating = Rating(
+        service_quality=request.POST['service_quality'],
+        cleanness=request.POST['cleanness'],
+        friendliness_of_staff=request.POST['friendliness_of_staff'],
+        rating_person=request.user
+    )
+    new_rating.save()
+    return redirect('hotel-page')
